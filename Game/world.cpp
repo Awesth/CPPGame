25052a0f89a4c709a1162b0cd5f6537438262854
awesth <apwesth@gmail.com>
2017-07-10 00:58:28 +0200

    Trying out a different way of creating the world, looks suboptimal

--- Game/world.cpp
+++ Game/world.cpp
@@ -7,29 +7,24 @@
 world::world() {}
 world::~world() {}
 
+std::string line = "______________________________";
+
 void world::creation(world buildmap)
 {
+
+   std::cout << line << "\n";
+
    for (int i = 0; i < buildmap.getxCord() ; ++i)
    {
       for (int j = 0; j < buildmap.getyCord(); ++j)
       {
-         if (buildmap.getPlace(i) == 1)
-         {
-            std::cout << " . ";
-         }
-         if (buildmap.getPlace(i) == 2)
-         {
-            std::cout << " H ";
-         }
-         if (buildmap.getPlace(i) == 3)
-         {
-            std::cout << " C ";
-         }
-         if (buildmap.getPlace(i) == 4)
-         {
-            std::cout << " F ";
-         }
+         std::cout << " " << buildmap.getPlace(i) << " ";
       }
+
    std::cout << "\n";
+
    }
+
+   std::cout << line << "\n";
+
 }
--- Game/world.h
+++ Game/world.h
@@ -12,7 +12,7 @@ class world
 private:
     int m_xCord = 10;
     int m_yCord = 10;
-    int m_place [10] = {1,1,2,1,1,3,1,4,1,1};
+    char m_place [10] = {'.','.','.','.','.','.','.','.','.','.'};
 
 public:
 
@@ -21,8 +21,9 @@ public:
     ~world();
     int getxCord() const { return m_xCord; }
     int getyCord() const { return m_yCord; }
-    int getPlace(int x) const { return m_place[x]; }
+    char getPlace(int x) const { return m_place[x]; }
     void creation(world);
+    std::string line = "______________________________";
 };
 
 
